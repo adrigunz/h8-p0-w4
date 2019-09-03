243f56f12ab1c44fd26aefb70fa23b86ccaf55e0
Adrian Adiguna Soeranto <adrigunz@gmail.com>
2019-09-03 13:52:54 +0700

    Create exercise-2.js
    upload exercise 2 week 4 to submit to foxhub

---
+++ exercise-2.js
@@ -0,0 +1,18 @@
+function fpb(angka1, angka2) {
+    // you can only write your code here!
+    var temp;
+    while ((angka1 % angka2) > 0) {
+        temp = angka1 % angka2;
+        angka1 = angka2;
+        angka2 = temp;
+    }
+    return angka2;
+}
+
+
+// TEST CASES
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1
